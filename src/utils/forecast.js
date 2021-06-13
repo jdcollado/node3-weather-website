86b4357400443e4dc865e8072e0b3c56db091cb5
Angelo Timajo <joselledcollado@gmail.com>
2021-06-13 18:36:45 +0800

    Add feelslike and humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -22,6 +22,8 @@
 
 const request = require('postman-request')
 
+
+
 const forecast = (longitude, latitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=1d42d4a22e90c491ea22eb7ca4ff57d3&query=' + latitude + ',' + longitude + '&units=m'
 
@@ -31,7 +33,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. There is " + body.current.precip + "% chance of rain.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out. The humidity is " + body.current.humidity + "%. There is " + body.current.precip + "% chance of rain.")
         }
     })
 }
